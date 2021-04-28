c35ababdd950424555704887e5f98041cbcc524d
professorisidro <isidro@professorisidro.com.br>
2021-04-28 14:50:24 -0300

    tela de relatorio finalizada

--- relatorio.js
+++ relatorio.js
@@ -4,6 +4,18 @@ function carregaInfo(){
         // volta pra tela de login
         window.location = "index.html";
     }
-    
+
+    // aqui começa o preenchimento das infos...
+    // agora maninpulo o usuario como um objeto
+    var usuario = JSON.parse(strUsuario);
+
+    document.getElementById("fotoUser").innerHTML = `<img src="${usuario.linkFoto}" width="100%">`;
+    document.getElementById("bioUser").innerHTML  = `<h4>${usuario.nome}</h4>
+                                                     <hr>
+                                                     <strong>Racf:</strong> ${usuario.racf} <br>
+                                                     <strong>Email:</strong> ${usuario.email} <br>
+                                                     <strong>Ramal:</strong> ${usuario.ramal}`;
+
+
 
 }
